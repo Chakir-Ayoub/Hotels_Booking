7c5b08f77f7e76eb1ab85da25b903881c35c41e8
Chakir Ayoub <ayoub.chakir0926@gmail.com>
2023-11-14 18:44:32 +0100

    Add compoent & Routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes } from '@angular/router';
+import { BodyComponent } from './designe/body/body.component';
+import { RouterModule } from '@angular/router';
+import { FacilitiesComponent } from './designe/facilities/facilities.component';
+import { RoomsComponent } from './designe/rooms/rooms.component';
+import { ContactUsComponent } from './designe/contact-us/contact-us.component';
+
+const routes: Routes=[
+  {path:"",redirectTo:"/home",pathMatch:'full'},
+  {path:'home',children:
+  [
+    {path:'',component:BodyComponent},
+    {path:'facilities',component:FacilitiesComponent},
+    {path:'rooms',component:RoomsComponent},
+    {path:'contact-us',component:ContactUsComponent}
+  ]}
+]
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes),
+    CommonModule,
+  ],
+  exports:[RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './designe/header/header.component';
+import { FooterComponent } from './designe/footer/footer.component';
+import { BodyComponent } from './designe/body/body.component';
+import { RoomsComponent } from './designe/rooms/rooms.component';
+import { FacilitiesComponent } from './designe/facilities/facilities.component';
+import { ContactUsComponent } from './designe/contact-us/contact-us.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BodyComponent,
+    RoomsComponent,
+    FacilitiesComponent,
+    ContactUsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
